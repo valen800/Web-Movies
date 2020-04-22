456a5910628f2a1bc2bd496927b7a69c1cf30c3f
valen800 <varulo.cvcv@gmail.com>
2020-04-22 13:04:28 +0200

    route system

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React from "react";
-import { Button } from "antd";
+import { layout, Layout } from "antd";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//Pages
+import Error404 from "./pages/error404";
+import Home from "./pages/home";
+import Movie from "./pages/movie";
+import NewMovies from "./pages/new-movies";
+import PopularMovies from "./pages/popular-movies";
+import Search from "./pages/search";
 
 function App() {
+  const { Header, Content } = Layout;
+
+  // Switch para cargar la primera pagina que encuentre si no carga dos parecidas por ejemplo
   return (
-    <div className="App">
-      <h1>Estamos en App</h1>
-    </div>
+    <layout>
+      <Router>
+        <Header>Header...</Header>
+        <Content>
+          <Switch>
+            <Route path="/" exact={true}>
+              <Home />
+            </Route>
+            <Route path="/new-movies" exact={true}>
+              <NewMovies />
+            </Route>
+            <Route path="/popular-movies" exact={true}>
+              <PopularMovies />
+            </Route>
+            <Route path="/search" exact={true}>
+              <Search />
+            </Route>
+            <Route path="/movie/:id" exact={true}>
+              <Movie />
+            </Route>
+            <Route path="*" exact={true}>
+              <Error404 />
+            </Route>
+          </Switch>
+        </Content>
+      </Router>
+    </layout>
   );
 }
 
