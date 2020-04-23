0b2c4a48cc644be2b5004a8685d54dc27ad93b13
valen800 <varulo.cvcv@gmail.com>
2020-04-23 12:46:37 +0200

    Popular Movies List

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Row, Col } from "antd";
 import useFetch from "../hooks/useFetch";
 import { URL_API, API } from "../utils/constants";
+import MovieList from "../components/MovieList";
 
 import SliderMovies from "../components/SliderMovies";
 
@@ -9,9 +11,19 @@ export default function Home() {
     `${URL_API}/movie/now_playing?api_key=${API}&language=es-ES&page=1`
   );
 
+  const popularMovies = useFetch(
+    `${URL_API}/movie/popular?api_key=${API}&language=es-ES&page=1`
+  );
+
   return (
     <>
       <SliderMovies movies={newMovies} />
+      <Row>
+        <Col span={12}>
+          <MovieList title="Películas Populares" movies={popularMovies} />
+        </Col>
+        <Col span={12}>...</Col>
+      </Row>
     </>
   );
 }
